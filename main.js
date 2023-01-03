c36416c46d1f3e62b0454078e988fb39656fe937
Andrea Gutierrez <ruth.andrea96@gmail.com>
2023-01-02 21:23:34 -0500

    En esta versión, se tomo la clase 13 la cual era el aside que muestra la lista de compra cuando se hace click sobre la imagen del carrtio en el navbar-right. Entonces, se agrego el evento de click del carrito de compras y tambien se pusieron los condicionales que cada vez que se haga click sobre cualquiera de los iconos del navbar sea en version desktop o mobile, los menus que estaban abiertos se escondan

--- main.js
+++ main.js
@@ -1,15 +1,44 @@
 const menuEmail =  document.querySelector('.navbar-email')
-const desktopMenu= document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+
+const desktopMenu= document.querySelector('.desktop-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) { //si el aside esta abierto, se le agrega la clase 'inactive' para que se cierre
+        aside.classList.add('inactive')
+    }
+
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed) { //si el aside esta abierto, se le agrega la clase 'inactive' para que se cierre
+        aside.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+    console.log('click');
+    
+    if (!isMobileMenuClosed) { //si el menu en mobile esta abierto, se le agrega la clase 'inactive' para que se cierre
+        mobileMenu.classList.add('inactive')
+    } 
+    else if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive')
+    }
+    aside.classList.toggle('inactive')
 }
